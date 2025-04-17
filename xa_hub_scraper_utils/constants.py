204945f614225bac0ef5de0461de263d0164e602
sRedRubyyy611 <178964539+redrubyyy611@users.noreply.github.com>
2025-04-16 23:11:58 -0400

    add some xa hub constants

--- xa_hub_scraper_utils/constants.py
+++ xa_hub_scraper_utils/constants.py
@@ -1,5 +1,27 @@
+# ======================
+# XA Hub group constants
+# ======================
+
+# IDS
 XA_HUB_GROUP_ID: int = 35310933
 """Roblox group ID for the XA Hub group."""
 
 XA_HUB_MEMBER_ROLE_ID: int = 234922010
 """Roblox role ID for the XA Hub member role."""
+
+XA_HUB_OWNER_ROBLOX_ID: int = 2953444466
+"""Roblox ID for the XA Hub owner."""
+
+XA_HUB_ADMIN_ROBLOX_IDS: list[int] = [5509421902, 2678492533]
+"""Roblox IDs for the XA Hub admins."""
+
+# Files
+XA_HUB_USERS_FILE: str = "data/xa_hub/xa_hub_users.txt"
+"""File path for the list of XA Hub usernames/user ID pairs text file."""
+
+XA_HUB_USER_IDS_JSON_FILE: str = "data/xa_hub/xa_hub_users.json"
+"""File path for the list of XA Hub users (raw Roblox group request data)."""
+
+XA_HUB_USER_IDS_TXT_FILE: str = "data/xa_hub/xa_hub_user_ids.txt"
+"""File path for the list of XA Hub user IDs text file."""
+
